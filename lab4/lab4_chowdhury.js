394b430618be750f3b77c6cf0f27e1c8a8fa9d28
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-02-11 14:36:26 -0500

    Create lab4_chowdhury.js

---
+++ lab4/lab4_chowdhury.js
@@ -0,0 +1,10 @@
+/**
+ * ehsan chowdhury
+ * Feb 11, function
+ */
+console.log("ehsan chowdhury")
+console.log("\n ---- example 1: function---")
+//create a function to prompt 'Hello World'
+function msg(){
+    console.log(`Hello World!`)
+}
