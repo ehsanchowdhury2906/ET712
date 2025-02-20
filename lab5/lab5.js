e0769c5b15ba75aee1ea3b5ca520e235c6f112fc
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-02-20 15:38:29 -0500

    Add files via upload

---
+++ lab5/lab5.js
@@ -0,0 +1,96 @@
+console.log("ehsan chowdhury")
+console.log("\n -- example 1: function in a variable ")
+const sum = function(num1,num2){
+    return num1+num2
+}
+// calling functions 
+console.log(sum(5,8))
+
+console.log("\n -- example 2 : unfction in a variable ")
+// function to return the sqaure root of a number
+let sqaurenumber = function(n){
+    return Math.pow(n,2)
+}
+// call function
+console.log(sqaurenumber(5))
+
+console.log("\n -- example 3 : arrow function")
+let greet = (username) => {
+    console.log(`Welcome to JS ${username}`)
+
+}
+// call fucntion 
+greet ("Peter Pan")
+
+console.log("\n -- example 4 : function with default parameter")
+function cubenumber(n){
+    return Math.pow(n,3)
+}
+// call function 
+console.log(`2^3 = ${cubenumber(2)}`)
+console.log(`empty^3 = ${cubenumber()}`)
+
+console.log("/n -- example spread syntax -- ")
+nums = [-2, 5, 9, 3, -8]
+let maxnum = Math.max(...nums)
+console.log(maxnum)
+
+console.log ("\n -- example 6 : objects -- ")
+// create an object
+const car ={
+    // properties
+    type: "fiat",
+    model: 500,
+    color:"white",
+    price: 23000,
+
+    // methods
+    description:function(){
+        return `${this.color} ${this.type} cost ${this.price}`  
+    }
+}
+// calling the object property 'model'
+console.log(car.model)
+// calling the object method 'description'
+console.log(car.description())
+
+console.log("\n -- example 7 : objects -- ")
+const hen = {
+    // properties
+    name: "Helen",
+    year: 2025,
+    eggcount : 0,
+
+    // method
+    layanegg: function(){
+        this.eggcount++
+        return "EGG"
+    }
+}
+//calling the property 'eggcount'
+console.log(`${hen.name} has ${hen.eggcount} eggs`)
+//calling the method
+console.log(hen.layanegg())
+console.log(hen.layanegg())
+console.log(`${hen.name} has ${hen.eggcount} eggs`)
+console.log(hen.layanegg())
+console.log(`${hen.name} has ${hen.eggcount} eggs`)
+
+console.log("\n -- example 8 : try-except")
+function yell(msg){
+    try{
+        console.log(msg.toUppercase().repeat(3))
+    }
+    catch{
+        console.log("ERROR!")
+    }
+    finally{
+        console.log("End of function 'yell")
+    }
+}
+// calling the function 
+yell("help")
+yell(8)
+console.log("end of example 8")
+
+console.log("\n -- exercise -- ")
