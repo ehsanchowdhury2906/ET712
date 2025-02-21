364021046e1bdb13807eea88cbd39f9804274ab8
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-02-21 00:02:30 -0500

    Update Ehsan.js

--- HW3/Ehsan.js
+++ HW3/Ehsan.js
@@ -4,7 +4,6 @@ Ehsan chowdhury
 Homework 3 – Objects and exception handling in JS 
 */  
 
-
 class Movie {
     constructor(title, director, year) {
         this.title = title;
@@ -12,24 +11,20 @@ class Movie {
         this.year = year;
     }
 
-    
     getDetails() {
         return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}`;
     }
 }
-
 
 class MovieCollection {
     constructor() {
         this.movies = []; 
     }
 
-    
     addMovie(movie) {
         this.movies.push(movie);
     }
 
-    
     removeMovie(title) {
         try {
             const index = this.movies.findIndex(movie => movie.title === title);
@@ -43,7 +38,6 @@ class MovieCollection {
         }
     }
 
-    
     showMovies() {
         try {
             if (this.movies.length === 0) {
@@ -57,30 +51,20 @@ class MovieCollection {
     }
 }
 
-
 const movieA = new Movie("The Batman", "Matt Reeves", 2022);
 const movieB = new Movie("Joker", "Todd Phillips", 2019);
 const movieC = new Movie("Avengers: Endgame", "Anthony and Joe Russo", 2019);
 
-
-
 const myCollection = new MovieCollection();
-
 
 myCollection.addMovie(movieA);
 myCollection.addMovie(movieB);
 myCollection.addMovie(movieC);
 
-
-myCollection.showMovies();
-
+myCollection.showMovies(); 
 
 myCollection.removeMovie("Joker");
-
-
 myCollection.removeMovie("The Batman");
-
 
 const emptyCollection = new MovieCollection();
 emptyCollection.showMovies();
-
