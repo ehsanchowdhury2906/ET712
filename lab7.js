221eccde63f3e016ae9edf8d0e69f6efd05160eb
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-03-12 17:48:58 -0400

    Update lab7.js

--- lab7.js
+++ lab7.js
@@ -51,3 +51,41 @@ btnsquare.addEventListener("click", function(){
 btnrectangle.addEventListener("click", function(){
     shape.className = "rectangle"
 })
+
+let event6 = document.querySelector(".event6")
+event6.onmouseout = function(){
+    alert("MOUSE OUT - Example 6")
+}
+function openalert(){
+    alert("CLICK EVEN - EXAMPLE 7")
+}
+//emample 8
+let divexample8 = document.querySelector(".divexample8")
+divexample8.onmouseout = function(){
+    let randcolor = Math.floor(Math.random()*255)
+    divexample8.computedStyleMap.backgroundColor = `rgb(${randcolor},${randcolor},${randcolor})`
+}
+
+let inputex9 = document.querySelector(".inputex9")
+let ex9paragraph = document.querySelector(".ex9paragraph")
+inputex9.onkeydown = function(){
+    ex9paragraph.innerHTML= `a key was pressed`
+}
+
+// Exercise
+
+document.querySelector(".ps1").onmouseleave = () => {
+    document.querySelector(".ps1").textContent = "EHSAN CHOWDHURY";
+};
+
+document.querySelector(".ps2").onclick = function() {
+    this.classList.toggle("ps3");
+};
+
+document.querySelector(".divs2").onclick = function() {
+    this.classList.toggle("divs4");
+};
+
+document.querySelector(".divs3").onkeydown = function() {
+    this.classList.toggle("divs5");
+};
