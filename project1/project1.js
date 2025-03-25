e5ec5d1da32a57296e63e26f9c6ea23e823f6ef7
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-03-25 19:52:53 -0400

    Update project1.js

--- project1/project1.js
+++ project1/project1.js
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic Image Slider</title>
+    <title>testing slider</title>
     <style>
         .slider-container {
             width: 100%;
