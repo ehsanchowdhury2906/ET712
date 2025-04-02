2b9f18d1a9612c07ea01d2a6603c4511cbb36e33
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-04-02 14:24:02 -0400

    Update project5.js

--- project5/project5.js
+++ project5/project5.js
@@ -1,17 +1,16 @@
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+// Wait for the page to load before adding event listeners
 document.addEventListener("DOMContentLoaded", function () {
- 
     let cards = document.querySelectorAll(".card");
 
-   
+    // Attach click event to each card
     cards.forEach(card => {
         card.addEventListener("click", function () {
-            let section = card.getAttribute("data-section");
-
-            // Navigate to the respective section (replace with actual URLs)
-            window.location.href = section + ".html";
-            window.location.href = section + ".html";
-            window.location.href = section + ".html";
-            window.location.href = section + ".html";
+            let pageName = this.getAttribute("data-page"); // Get target page from data attribute
+            navigateTo(pageName);
         });
     });
 });
