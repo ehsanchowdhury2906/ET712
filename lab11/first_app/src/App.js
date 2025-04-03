24f873333780fe7d8ef71967f6b472062bd776e2
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-04-03 19:35:50 +0000

    yes

--- lab11/first_app/src/App.js
+++ lab11/first_app/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './App.css';
+import kaabaimg from'./';
 
 function App() {
-  return (
-    <div className="App">
+  const msg = "Welcome to ReactJS"
+    function printSomething(){
+      return "something"
+    }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
