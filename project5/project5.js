cbdb17b4b239911e9cbd23c84a63d4cb484df9a7
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-04-06 03:19:21 -0400

    Update project5.js

--- project5/project5.js
+++ project5/project5.js
@@ -97,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     if (document.querySelector("#shape-image")) {
         let shapesData = [
-            { image: "https://via.placeholder.com/200x200?text=Circle", answer: "circle" },
+            { image: "https://img.freepik.com/free-vector/stroke-round-geometric-shape-vector_53876-175080.jpg?semt=ais_hybrid&w=740", answer: "circle" },
             { image: "https://via.placeholder.com/200x200?text=Square", answer: "square" },
             { image: "https://via.placeholder.com/300x200?text=Rectangle", answer: "rectangle" }
         ];
