b1a883c6ed7e3817fe761928fe30c6eb6b4c5b2f
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-04-08 20:00:17 +0000

    lab12

--- lab12/lab12/lab12/props_react/src/App.js
+++ lab12/lab12/lab12/props_react/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import user from "./comments.js"
 
 function App() {
   return (
---
+++ lab12/lab12/lab12/props_react/src/comments.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index"
+import guy from "2291005_face_fun_funny_joking_smile_icon.svg"
+
+const User = function(){
+    return(
+        <>
+            <h1>User comments</h1>
+            <section className="card"
+        </>
+    )
+}
+
+export default User;
