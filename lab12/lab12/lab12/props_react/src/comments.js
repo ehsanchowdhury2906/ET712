49115db5de96bdd25dd10cad4a2272dde0b3bc24
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-04-18 01:29:27 -0400

    Update comments.js

--- lab12/lab12/lab12/props_react/src/comments.js
+++ lab12/lab12/lab12/props_react/src/comments.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./index"
-import guy from "2291005_face_fun_funny_joking_smile_icon.svg"
+import "./index.css"
 
-const User = function(){
+const User = function(props){
     return(
         <>
-            <h1>User comments</h1>
-            <section className="card"
+            <section className="card">
+                <img src={props.image}/>
+                <div className="content">
+                    <a className="linkauthor">{props.username}</a>
+                </div>
+                <div className="metadata">
+                    <span className="postdate">{props.date}</span>
+                </div>
+                <div className="comment">
+                    <p>{props.usercomment}</p>
+                </div>
+            </section>
         </>
     )
 }
 
-export default User;
+export default User; 
