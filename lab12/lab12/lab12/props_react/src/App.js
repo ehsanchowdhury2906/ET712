6610eff2cb6c16ccd8d05fcb060a75d47c92f5a3
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-04-18 03:13:59 -0400

    Update App.js

--- lab12/lab12/lab12/props_react/src/App.js
+++ lab12/lab12/lab12/props_react/src/App.js
@@ -7,18 +7,18 @@ import modalwindow from "./modalwindow"
 
 
 //import images
-import cat from "./images/cat.png"
-import chicken from "./images/chicken.png"
-import panda from "./images/panda.png"
+import china from "./5546688_china_asia_chinese_circle_country_icon"
+import clown from "./2411817_clown_emoji_emoticon_funny_happy_icon"
+import fun from "./2291005_face_fun_funny_joking_smile_icon"
 
 function App() {
   return (
    <>
    <h1 style={{textAlign:"center"}}>toni chen liang</h1>
    <main className="maincontainer">
-    < User_feedback><User image={cat} username="Mr. cat" date="04/08/25" usercomment="great job"/></User_feedback>
-    < User_feedback><User image={chicken} username="Mr. chicken" date="04/08/25" usercomment="glad to help"/></User_feedback>
-    < User_feedback><User image={panda} username="Mr. panda" date="04/08/25" usercomment="relax"/></User_feedback>
+    < User_feedback><User image={china} username="Mr. cat" date="04/08/25" usercomment="great job"/></User_feedback>
+    < User_feedback><User image={clown username="Mr. chicken" date="04/08/25" usercomment="glad to help"/></User_feedback>
+    < User_feedback><User image={fun} username="Mr. panda" date="04/08/25" usercomment="relax"/></User_feedback>
 
    </main>
 
