3c14af403f593cdaa0c8ffa80724b194e6db2083
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-04-20 18:56:26 -0400

    Update index.js

--- lab12/lab12/lab12/props_react/src/index.js
+++ lab12/lab12/lab12/props_react/src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import bati from "./images/bati.png";
+import musi from "./images/musi.png";
+import musia from "./images/musia.png";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import User_feedback from './user_feedback';
+import './index.css';
+import User from './comments'
+
+const App = function () {
+  return (
+  <>
+  <h1 style={{ textAlign: "center" }}>ehsan chowdhury</h1>
+  <section className='container'>
+  {/* user 1 */}
+  <User_feedback username="bati"><User image={bati}
+  date="11/12/24" msg="I am Batman" /></User_feedback>
+  {/* user 2 */}
+  <User_feedback username="musi"><User image={musi} date="10/03/24"
+  msg="I am late!" /></User_feedback>
+  {/* user 3 */}
+  <User_feedback username="musia"><User image={musia} date="08/20/24"
+  msg="Get more rest" /></User_feedback>
+  </section>
+  {/* POST COMMENTS */}
+  <div className="postcomment">
+  <ul className="commentlist"></ul>
+  </div>
+  </>
+  )
+  }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +38,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
