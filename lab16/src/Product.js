3e03b16aa9378155ce66b9c1d0c7c0025d98bc68
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-05-04 16:44:52 -0400

    Update Product.js

--- lab16/src/Product.js
+++ lab16/src/Product.js
@@ -2,13 +2,13 @@ import React from "react";
 import './App.css';
 
 function Product({ product, onAddToCart }) {
-    return (
-      <div className="product">
-        <h3>{product.name}</h3>
-        <p>${product.price.toFixed(2)}</p>
-        <button onClick={onAddToCart}>Add to Cart</button>
-      </div>
-    );
-  }
-  
-  export default Product;
+  return (
+    <div className="product">
+      <h3>{product.name}</h3>
+      <p>${product.price.toFixed(2)}</p>
+      <button onClick={onAddToCart}>Add to Cart</button>
+    </div>
+  );
+}
+
+export default Product;
