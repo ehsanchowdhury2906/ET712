e22fe88e30f4d1d6abc293a2b87bf9fa09dd2231
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-05-04 16:43:49 -0400

    Update Navbar.js

--- lab16/src/Navbar.js
+++ lab16/src/Navbar.js
@@ -1,16 +1,18 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import './App.css';
 
 function Navbar({ cartCount }) {
-    return (
-      <nav className="navbar">
-        <Link to="/" className="home">Home</Link>
-        <Link to="/cart" className="cart">
-          Cart ({cartCount})
-        </Link>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
+  return (
+    <nav className="navbar">
+      <Link to="/" className="home">
+        Home
+      </Link>
+      <Link to="/cart" className="cart">
+        Cart ({cartCount})
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
